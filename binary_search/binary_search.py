3b66397da863497de9549961522de9627c4269c8
kuganveluswamy <kuganveluswamy@gmail.com>
2021-01-03 13:14:32 -0800

    rectified the indensation error

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -26,27 +26,27 @@ def binary_search(mylist, data):
   return -1
 
 if __name__ == "__main__":
-# Creates input list of numbers for the binary search algorithm
-# - list of random integers in a range
-# - remove the redundant elements and sort elements using set()
-list_size = 100
-number_list = [randint(1,list_size) for i in range(list_size)]
-number_list = list(set(number_list))
-print("The input list of numbers: {}".format(number_list))
-print("The length of list of numbers: {}\n".format(len(number_list)))
+  # Creates input list of numbers for the binary search algorithm
+  # - list of random integers in a range
+  # - remove the redundant elements and sort elements using set()
+  list_size = 100
+  number_list = [randint(1,list_size) for i in range(list_size)]
+  number_list = list(set(number_list))
+  print("The input list of numbers: {}".format(number_list))
+  print("The length of list of numbers: {}\n".format(len(number_list)))
 
-# Creates input list of names for the binary search algorithm
-# - join random alpabets to form a word
-# - list of random words and sorted 
-list_size = 100
-word_size = 5
-word_list = [''.join( choice(ascii_lowercase) for i in range(word_size)) for i in range(list_size)]
-word_list = list(set(word_list))
-word_list.sort()
-print("The input list of words: {}".format(word_list))
-print("The length of list of words: {}\n".format(len(word_list)))
+  # Creates input list of names for the binary search algorithm
+  # - join random alpabets to form a word
+  # - list of random words and sorted 
+  list_size = 100
+  word_size = 5
+  word_list = [''.join( choice(ascii_lowercase) for i in range(word_size)) for i in range(list_size)]
+  word_list = list(set(word_list))
+  word_list.sort()
+  print("The input list of words: {}".format(word_list))
+  print("The length of list of words: {}\n".format(len(word_list)))
 
-# Testing with number_list  
+  # Testing with number_list  
   test_num = 150
   print("Position of {} in number_list: {}".format(test_num, binary_search(number_list, test_num)))	# expected -1
   for i in range(5):
@@ -54,7 +54,7 @@ print("The length of list of words: {}\n".format(len(word_list)))
     print("Position of {} in number_list: {}".format(test_num, binary_search(number_list, test_num)))
   print("\n")
 
-# Testing with word_list  
+  # Testing with word_list  
   test_word = 'abcdef'
   print("Position of {} in word_list: {}".format(test_word, binary_search(word_list, test_word))) 	# expected -1
   for i in range(5):
